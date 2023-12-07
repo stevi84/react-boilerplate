86151dd81efea1ff03a41a2a703b1d5367c10385
Stefan Leupold <stefan.leupold@salt-solutions.de>
2023-12-07 14:11:31 +0100

    Readme aktualisiert, Language detector entfernt

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,20 +1,11 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { defaultLocale, translations } from './globals/Translations';
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: translations,
-    fallbackLng: defaultLocale,
-    detection: {
-      order: ['querystring', 'localStorage'],
-      lookupQuerystring: 'lng',
-      lookupLocalStorage: 'i18nextLng',
-      caches: ['localStorage'],
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources: translations,
+  lng: defaultLocale,
+  fallbackLng: defaultLocale,
+});
 
 export { i18n };
